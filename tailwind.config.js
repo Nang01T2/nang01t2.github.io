82520b17a9859d5ba52e4e8c6458070ad55857ba
Nang Nguyen <thaonguyen83.dn@gmail.com>
2023-04-10 17:35:16 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
+  plugins: [require("@tailwindcss/typography")],
 };
