e04f39f7caa97b9c81370f389d8ab3910baed4b0
Nang Nguyen <thaonguyen83.dn@gmail.com>
2023-04-17 11:39:24 +0700

    Update

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,14 @@
+import PageTitle from "./PageTitle";
+
+export default function UnderConstruction() {
+  return (
+    <div className="mt-24 text-center">
+      <PageTitle>
+        Under Construction <br />
+        <span role="img" aria-label="roadwork sign">
+          🚧
+        </span>
+      </PageTitle>
+    </div>
+  );
+}
--- src/pages/posts/[...id].js
+++ src/pages/posts/[...id].js
@@ -1,14 +1,15 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents';
+import UnderConstruction from "@/components/UnderConstruction";
 
-import { formatSlug, getFiles, getFileBySlug } from '@/lib/get-posts-data';
+import { formatSlug, getFiles, getFileBySlug } from "@/lib/get-posts-data";
 
 export async function getStaticPaths() {
-  const blogFiles = getFiles('blog');
+  const blogFiles = getFiles("blog");
   const paths = blogFiles.map((p) => {
     //console.log('file: ', formatSlug(p).split('/'));
     return {
       params: {
-        id: formatSlug(p).split('/'),
+        id: formatSlug(p).split("/"),
       },
     };
   });
@@ -20,7 +21,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getFileBySlug('blog', params?.id.join('/'));
+  const postData = await getFileBySlug("blog", params?.id.join("/"));
   return {
     props: {
       ...postData,
@@ -29,20 +30,24 @@ export async function getStaticProps({ params }) {
 }
 
 export default function PostDetail({ metadata, content, toc }) {
-  console.log('TOC', toc);
-  console.log('Metadata', metadata);
+  console.log("TOC", toc);
+  console.log("Metadata", metadata);
   //console.log('content', content);
   return (
-    <div>
-      <MDXLayoutRenderer
-        layout={metadata.layout || 'BlogLayout'}
-        toc={toc}
-        mdxSource={content}
-        frontMatter={metadata}
-        //authorDetails={authorDetails}
-        //prev={prev}
-        //next={next}
-      />
-    </div>
+    <>
+      {metadata?.draft !== true ? (
+        <MDXLayoutRenderer
+          layout={metadata.layout || "BlogLayout"}
+          toc={toc}
+          mdxSource={content}
+          frontMatter={metadata}
+          //authorDetails={authorDetails}
+          //prev={prev}
+          //next={next}
+        />
+      ) : (
+        <UnderConstruction />
+      )}
+    </>
   );
 }
