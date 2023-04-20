72f15455f7e819082767e500829cdff76da75838
Nang Nguyen <thaonguyen83.dn@gmail.com>
2023-04-20 17:53:46 +0700

    Update

---
+++ src/components/common/Title.js
@@ -0,0 +1,13 @@
+import { $ } from "@/libs/core";
+
+export default function Title({ className, ...props }) {
+  return (
+    <h1
+      {...props}
+      className={$(
+        "mb-4 text-3xl font-extrabold tracking-tight sm:text-5xl",
+        className
+      )}
+    />
+  );
+}
---
+++ src/pages/snippets/index.js
@@ -0,0 +1,11 @@
+import Title from "@/components/common/Title";
+import Layout from "@/components/layouts/Layout";
+import React from "react";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Title>Code Snippets</Title>
+    </Layout>
+  );
+}
